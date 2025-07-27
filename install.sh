f3b6ac9a1c366ea1ee31b6ddbb92fa0060fd4eaa
Stephen Baraik <stephenbaraik@gmail.com>
2025-07-27 13:14:55 +0530

    revised code

--- install.sh
+++ install.sh
@@ -13,16 +13,16 @@ rm ~/hadoop-3.4.0.tar.gz
 mv ~/hadoop-3.4.0 ~/hadoop
 
 echo "[INFO] Setting up environment variables..."
-cat ~/hadoop-wsl-installer/config/.bashrc.append >> ~/.bashrc
-cp ~/hadoop-wsl-installer/config/hadoop-profile.sh ~/hadoop/
+cat ~/hadoop-wsl-setup/config/.bashrc.append >> ~/.bashrc
+cp ~/hadoop-wsl-setup/config/hadoop-profile.sh ~/hadoop/
 source ~/.bashrc
 
 echo "[INFO] Configuring Hadoop..."
-cp ~/hadoop-wsl-installer/scripts/*.xml ~/hadoop/etc/hadoop/
-cp ~/hadoop-wsl-installer/scripts/hadoop-env.sh ~/hadoop/etc/hadoop/
+cp ~/hadoop-wsl-setup/scripts/*.xml ~/hadoop/etc/hadoop/
+cp ~/hadoop-wsl-setup/scripts/hadoop-env.sh ~/hadoop/etc/hadoop/
 
 echo "[INFO] Formatting HDFS..."
 ~/hadoop/bin/hdfs namenode -format
 
 echo "[INFO] Hadoop installation complete!"
-echo "Run: start-dfs.sh && start-yarn.sh"
+echo "Run: start-hdfs.sh && start-yarn.sh"
